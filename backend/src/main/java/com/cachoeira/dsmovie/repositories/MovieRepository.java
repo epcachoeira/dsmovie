44187cac5f96e9a620ad21b39d92a35347382a4e
Eduardo Cachoeira <epcachoeira@hotmail.com>
2022-03-09 14:14:25 -0300

    Find movies

---
+++ backend/src/main/java/com/cachoeira/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.cachoeira.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cachoeira.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
